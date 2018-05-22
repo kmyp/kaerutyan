855151a2a11a186bd66186d9d1a446fd71327a1a
kim <kim@133.68.94.105>
2018-05-23 00:40:06 +0900

    60%complete

--- kaerutyan.py
+++ kaerutyan.py
@@ -1,7 +1,16 @@
 '''
 Created on 2018/04/25
-last edit 2018/5/21
+last edit 2018/5/22
 @author: kim
+GUIベースで遅延とパケット損失を設定できる仕様
+bridge773があるかどうかは確認
+なければ起動しない
+
+###ToDo
+ファイルの入出力
+パイプやブリッジがもしなかったらの処理は暇なときにやる
+
+
 '''
 import cv2
 import time
@@ -16,6 +25,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5.Qt import QPalette
 import subprocess
+from symbol import except_clause
 
 class EnvChanger(QMainWindow):
     def __init__(self, parent=None,initials=None):
@@ -24,7 +34,9 @@ class EnvChanger(QMainWindow):
         self.font = QFont('Serif', 22, QFont.Light)
         self.ret_flg = False
 
-        self.env_num = 0
+        self.env_num = -1
+        self.delay_num = -1
+        self.plr_num = -1
 
         # Menu Bar
         self.file_menu = self.menuBar().addMenu("&File")
@@ -76,7 +88,7 @@ class EnvChanger(QMainWindow):
 
         self.add_button = self.put_add_button()
         self.apply_button = self.put_apply_button()
-
+        self.delete_button = self.put_delete_button()
 
 
 
@@ -113,7 +125,7 @@ class EnvChanger(QMainWindow):
         btn = QPushButton("add", self)
         btn.setFont(QFont('Serif', 18, QFont.Light))
         btn.setStyleSheet("color : blue")
-        btn.setGeometry(30, 185, 70, 35)
+        btn.setGeometry(8, 185, 70, 35)
         btn.setEnabled(True)
         btn.clicked.connect(self.button_event_add)
         return btn
@@ -127,7 +139,7 @@ class EnvChanger(QMainWindow):
             self.env_list_table.setRowCount(len(self.env_list))
             i = 0
             for k,v in sorted(self.env_list.items(), key = lambda x:x[0]):
-                self.env_list_table.setItem(0, i, QTableWidgetItem("【"+str(k)+"】"+"  " + str(v)))
+                self.env_list_table.setItem(0, i, QTableWidgetItem(str(k)+".  " + str(v)))
                 print(str(v))
                 i = i + 1
 
@@ -135,8 +147,8 @@ class EnvChanger(QMainWindow):
     def put_apply_button(self):
         btn = QPushButton("apply", self)
         btn.setFont(QFont('Serif', 18, QFont.Light))
-        btn.setStyleSheet("color : red")
-        btn.setGeometry(110, 185, 70, 35)
+        btn.setStyleSheet("color : green")
+        btn.setGeometry(85, 185, 70, 35)
         btn.setEnabled(True)
         btn.clicked.connect(self.button_event_apply)
         return btn
@@ -145,21 +157,57 @@ class EnvChanger(QMainWindow):
         try:
             if len(self.env_list_table.selectedItems())>0:
                 cmd = self.env_list_table.selectedItems()[0].text().split("  ")
-                if len(cmd < 2):
+                if len(cmd) < 2:
                     raise Exception("cmd error") #ToDo 例外上げる
-                print(cmd[1])
-                subprocess.check_call("ipfw pipe 773")
+                print("ipfw pipe 773 config "+ cmd[1])
+                subprocess.check_call("ipfw pipe 773 config "+ cmd[1])
+                self.statusBar().showMessage("setting " + cmd[0])
 
+            else :
+                print("please select environment")
 
         except Exception as e:
-            print(e)
+            print("ipfwコマンドの実行に失敗しました。")
+            self.statusBar().showMessage("failed to excute ipfw")
+            sub = SubWindow(self)
+            sub.show()
+
+        finally:
+            self.env_list_table.clearSelection()
+
+    def put_delete_button(self):
+        btn = QPushButton("delete", self)
+        btn.setFont(QFont('Serif', 18, QFont.Light))
+        btn.setStyleSheet("color : red")
+        btn.setGeometry(163, 185, 70, 35)
+        btn.setEnabled(True)
+        btn.clicked.connect(self.button_event_delete)
+        return btn
+
+    def button_event_delete(self):
+        try:
+            if len(self.env_list_table.selectedItems())>0:
+                cmd = self.env_list_table.selectedItems()[0].text().split("  ")
+                if len(cmd) < 2:
+                    raise Exception("faital error") #ToDo 例外上げる
+                index = int(cmd[0].replace(".",""))
+                del self.env_list[index]
+                self.env_list_table.setRowCount(len(self.env_list))
+                i = 0
+                for k,v in sorted(self.env_list.items(), key = lambda x:x[0]):
+                    self.env_list_table.setItem(0, i, QTableWidgetItem(str(k)+".  " + str(v)))
+                    print(str(v))
+                    i = i + 1
+                self.env_list_table.clearSelection()
+            else :
+                print("please select environment")
+
+        except Exception as e:
+            print(e.message)
             qApp.quit()
 
 
-
-
     def on_env_text_input(self,text):
-        print(text)
         if text.isnumeric():
             self.env_num = int(text)
         else :
@@ -167,7 +215,6 @@ class EnvChanger(QMainWindow):
             self.env_num = -1
 
     def on_delay_text_input(self,text):
-        print(text)
         if text.isnumeric():
             self.delay_num = int(text)
         else :
@@ -175,12 +222,42 @@ class EnvChanger(QMainWindow):
             self.delay_num = -1
 
     def on_plr_text_input(self,text):
-        print(text)
         if text.isnumeric():
             self.plr_num = int(text)/100
         else :
             self.statusBar().showMessage("Invalid input:plr")
             self.plr_num = -1
+
+
+
+class SubWindow:
+    def __init__(self, parent=None):
+        self.w = QDialog(parent)
+        self.parent = parent
+        self.font = QFont('Serif', 22, QFont.Light)
+
+        label = QLabel()
+        label.setText('ipfw excution FAILED')
+        label.setFont(self.font)
+        label.setStyleSheet("color: red")
+        btn = QPushButton('Exit')
+        btn.clicked.connect(self.w.close)
+        btn.setFont(QFont("Serif", 14, QFont.Light))
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(label)
+        btn_layout = QHBoxLayout()
+        btn_layout.addWidget(btn)
+        main_layout.addLayout(btn_layout)
+
+        self.w.setLayout(main_layout)
+
+        self.w.setGeometry(320, 320, 280, 160)
+
+    def show(self):
+        self.w.exec_()
+
+
+
 
 
 def create_main_window(argv):
@@ -196,7 +273,10 @@ def create_main_window(argv):
 
 
 if __name__ == "__main__":
-    if u'bridge773' in netifaces.interfaces():
+
+    print("実験環境カエルちゃんのバージョン0.0.1です。\n未実装な部分および未知のバグが多いので注意してください。\nブリッジとパイプは事前に手動で設定してください。\n\n")
+    if u'bridge773' in netifaces.interfaces() or True:
         create_main_window(sys.argv)
+#     elif subprocess.call("ifconfig bridge773 create".split("" )):
     else:
         print("ごめんね、、私はそんなに高性能じゃないから、bridge773の設定をしてから再度起動してください。。。")
